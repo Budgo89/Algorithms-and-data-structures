0a7c712ff56fe43e7d2aa4f0bcc774bb65d9fcb2
Budgo <Budgo@mail.ru>
2021-04-14 12:41:06 +0300

    Создание проекта

---
+++ Algorithms and data structures/Algorithms and data structures/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algorithms_and_data_structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
