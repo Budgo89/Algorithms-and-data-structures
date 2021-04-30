657680915746488b84bb1aeba5b8b40b4fd96f98
Budgo <Budgo@mail.ru>
2021-04-30 17:26:22 +0300

    no message

---
+++ Algorithms and data structures/Lesson_6/Graph.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_6
+{
+    public class Graph
+    {
+        public Node node;
+
+    public class Node //Вершина
+        {
+            public int Value { get; set; }
+            public List<Edge> Edges { get; set; } //исходящие связи
+
+            public Node BFSGraph(int x, Node _node)
+            {
+                var bufer = new Queue<Node>();
+                var knot = new Queue<Node>();
+                bufer.Enqueue(_node);
+                Console.WriteLine($"Добавляем стартовую ноду {_node.Value} в очередь");
+                while (bufer.Count != 0)
+                {
+                    var root = bufer.Dequeue();
+                    Console.WriteLine($"Вытягиваем ноду {root.Value} из начала очереди и удаляем из списка");
+
+                    if (root != null)
+                    {
+                        Console.WriteLine($"Сравниваем с {x}");
+                        if (root.Value == x)
+                            return root;
+                        Console.WriteLine($"Удаляем ноду {root.Value} из стэка");
+                        knot.Enqueue(root);
+                        Console.WriteLine($"Отмечаем ноду {root.Value} как проверенную");
+                        for (int i = 0; i < root.Edges.Count; i++)
+                        {
+                            var a = root.Edges[i];
+                            bufer.Enqueue(a.Node);
+                            Console.WriteLine($"Добавляем в очередь ноду {a.Node.Value}");
+                        }
+                        if (bufer.Count != 0 && knot.Count != 0)
+                        {
+                            while (bufer.Peek() == knot.Peek())
+                            {
+                                bufer.Dequeue();
+                            }
+                            Console.WriteLine("Удаляем проверенные элементы из буфера");
+                        }
+                    }
+                }
+                Console.WriteLine("Элемент не найдев, возвращаю стартавый элемент: ");
+                return _node;
+            }
+
+            public Node DFSGraph(int x, Node _node)
+            {
+
+                var stek = new Stack<Node>();
+                var knot = new Queue<Node>();
+                stek.Push(_node);
+                Console.WriteLine($"Добавляем стартовую ноду {_node.Value} в стэк");
+                while (stek.Count != 0)
+                {
+                    var root = stek.Pop();
+                    Console.WriteLine($"Вытягиваем ноду {root.Value} и удалем из стека");
+                    if (root != null)
+                    {
+                        Console.WriteLine($"Сравниваем с {x}");
+                        if (root.Value == x)
+                            return root;
+                        knot.Enqueue(root);
+                        Console.WriteLine($"Отмечаем ноду {root.Value} как проверенную");
+                        for (int i = root.Edges.Count - 1; i >= 0; i--)
+                        {
+                            var a = root.Edges[i];
+                            stek.Push(a.Node);
+                            Console.WriteLine($"Добавляем в стек ноду {a.Node.Value}");
+                        }
+                        if (stek.Count != 0 && knot.Count != 0)
+                        {
+                            while (stek.Peek() == knot.Peek())
+                            {
+                                stek.Pop();
+                            }
+                            Console.WriteLine("Удаляем проверенные элементы из стека");
+                        }
+                    }
+                }
+                Console.WriteLine("Элемент не найдев, возвращаю стартавый элемент: ");
+                return _node;
+            }
+        }
+
+        public class Edge //Ребро
+        {
+            public int Weight { get; set; } //вес связи
+            public Node Node { get; set; } // узел, на который связь ссылается            
+        }
+        
+
+    }
+}
---
+++ Algorithms and data structures/Lesson_6/Program.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson_6
+{
+    class Program
+    {
+        public class TestCase
+        {
+            public Graph.Node node { get; set; }
+            public int X { get; set; }
+            public int searchValueInt { get; set; }
+            public Exception ExpectedException { get; set; }
+        }
+
+        static void TestBFS(TestCase testCase)
+        {
+            try
+            {
+                var a = testCase.node.BFSGraph(testCase.X, testCase.node) ;
+                if (a.Value == testCase.searchValueInt)                
+                {
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+            }
+            catch (Exception)
+            {
+
+                if (testCase.ExpectedException != null)
+                {
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+            }
+        }
+        static void TestDFS(TestCase testCase)
+        {
+            try
+            {
+                var a = testCase.node.DFSGraph(testCase.X, testCase.node);
+                if (a.Value == testCase.searchValueInt)
+                {
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+            }
+            catch (Exception)
+            {
+
+                if (testCase.ExpectedException != null)
+                {
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+            }
+        }
+        public static Graph.Node GenerGraph()
+        {
+            var s = new Graph();
+            Graph.Node x = new Graph.Node()
+            {
+                Value = 66,
+                Edges = new List<Graph.Edge>()
+            };
+            x.Edges.Add(new Graph.Edge
+            {
+                Weight = 7,
+                Node = new Graph.Node
+                {
+                    Value = 8,
+                    Edges = new List<Graph.Edge>()
+                }
+            });
+            x.Edges.Add(new Graph.Edge
+            {
+                Weight = 8,
+                Node = new Graph.Node
+                {
+                    Value = 1,
+                    Edges = new List<Graph.Edge>()
+                }
+            });
+            x.Edges.Add(new Graph.Edge
+            {
+                Weight = 2,
+                Node = new Graph.Node
+                {
+                    Value = 10,
+                    Edges = new List<Graph.Edge>()
+                }
+            });
+            x.Edges[0].Node.Edges.Add(new Graph.Edge
+            {
+                Weight = 3,
+                Node = new Graph.Node
+                {
+                    Value = 3,
+                    Edges = new List<Graph.Edge>()
+                }
+            });
+            x.Edges[1].Node.Edges.Add(new Graph.Edge
+            {
+                Weight = 4,
+                Node = new Graph.Node
+                {
+                    Value = 4,
+                    Edges = new List<Graph.Edge>()
+                }
+            });
+            x.Edges[2].Node.Edges.Add(new Graph.Edge
+            {
+                Weight = 9,
+                Node = new Graph.Node
+                {
+                    Value = 9,
+                    Edges = new List<Graph.Edge>()
+                }
+            });
+            x.Edges[2].Node.Edges.Add(new Graph.Edge
+            {
+                Weight = 2,
+                Node = new Graph.Node
+                {
+                    Value = 2,
+                    Edges = new List<Graph.Edge>()
+                }
+            });
+            x.Edges[0].Node.Edges[0].Node.Edges.Add(new Graph.Edge
+            {
+                Weight = 3,
+                Node = new Graph.Node
+                {
+                    Value = 3,
+                    Edges = new List<Graph.Edge>()
+                    {
+                        new Graph.Edge
+                        {
+                            Node = x.Edges[1].Node.Edges[0].Node,
+                            Weight = 20
+                        }
+
+                    }
+                }
+            });
+            return x;
+        }
+
+        static void Main(string[] args)
+        {
+            var x = GenerGraph();
+
+
+            var test1 = new TestCase
+            {
+                node = x,
+                X = 4,
+                searchValueInt = 4,
+                ExpectedException = null,
+
+            };
+            TestBFS(test1);
+            TestDFS(test1);
+            var test2 = new TestCase
+            {
+                node = x.Edges[1].Node,
+                X = 4,
+                searchValueInt = 4,
+                ExpectedException = null,
+
+            };
+            TestBFS(test2);
+            TestDFS(test2);
+            var test3 = new TestCase
+            {
+                node = x.Edges[2].Node,
+                X = 4,
+                searchValueInt = 4,
+                ExpectedException = null,
+
+            };
+            TestBFS(test3);
+            TestDFS(test3);
+            Console.ReadKey();
+        }
+    }
+}
